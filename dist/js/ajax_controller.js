993dd86a0755c8c3d566d4c945ee2977a69fee02
Anya Novikova <pesik@ane4k.in>
2016-08-19 00:48:42 +0300

    Ajax code updates for microservice.

--- dist/js/ajax_controller.js
+++ dist/js/ajax_controller.js
@@ -1,9 +1,23 @@
 /**
  * Created by pesik@ane4k.in on 17.08.16.
  */
+service_uri = "http://ane4k.in:8080/travel";
+
 window.conrollers = {
     ajax_form_controller: function (data) {
-        console.log("validating");
-        console.log(data);
+        console.log("sending", data);
+
+        $.post(service_uri, JSON.stringify(data), successHandler, "json").fail(function(e) {
+            console.warn( e.responseText );
+        });
     }
+};
+
+successHandler = function (data) {
+    console.log("Callback is: ", data);
+
+    console.log(data["status"]);
+    console.log(data["username"]);
+
+    alert(data["username"]);
 };
