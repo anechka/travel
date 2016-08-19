63cbced7f8353ce606a0d5abaed6dfec6f51ee53
Anya Novikova <pesik@ane4k.in>
2016-08-20 01:49:29 +0300

    Form validation bug fixing with riot virtual DOM

--- dist/js/form.jsx
+++ dist/js/form.jsx
@@ -3,38 +3,44 @@
  */
 <form-tag>
 
-    <form onsubmit="{ submit }" if="{ !flag }" id="conversationForm">
-        <div class="form-group">
-            <label for="InputEmail" class="sr-only">Your email</label>
-            <input id="InputEmail" type="email" placeholder="Your email" class="form-control" required>
+    <form if="{ !flag }" id="conversationForm">
+        <div class="form-group has-feedback">
+            <label for="inputEmail" class="sr-only">Your email</label>
+            <div class="input-group">
+                <span class="input-group-addon">@</span>
+                <input id="inputEmail" class="form-control" type="email" placeholder="Your email" data-error="Bruh, that email address is invalid" required>
+            </div>
+            <span class="glyphicon form-control-feedback" aria-hidden="true"></span>
+            <div class="help-block with-errors"></div>
         </div>
         <div class="form-group">
-            <label for="userName" class="sr-only">Name</label>
-            <input id="userName" type="text" placeholder="Name" class="form-control" required>
+            <label class="sr-only" for="userName">Name</label>
+            <input class="form-control" id="userName" type="text" placeholder="Name" data-minlength="3" required>
+            <div class="help-block with-errors"></div>
         </div>
         <label class="radio-inline">
-            <input id="Radio1" type="radio" name="inlineRadioOptionsSex" value="option1" required> Female
+            <input id="femaleRadio" type="radio" name="inlineRadioOptionsSex" value="female" required> Female
         </label>
         <label class="radio-inline">
-            <input id="Radio2" type="radio" name="inlineRadioOptionsSex" value="option2"> Male
+            <input id="maleRadio" type="radio" name="inlineRadioOptionsSex" value="male"> Male
         </label>
         <div class="form-group margin-top-15">
-            <label for="InputCity" class="sr-only">City/State</label>
-            <input id="InputCity" type="text" placeholder="City/State" class="form-control" required>
+            <label for="inputCity" class="sr-only">City/State</label>
+            <input id="inputCity" type="text" placeholder="City/State" class="form-control" required>
         </div>
         <div class="form-group">
-            <label for="Inputcountry" class="sr-only">Country</label>
-            <input id="Inputcountry" type="tetx" placeholder="Country" class="form-control" required>
+            <label for="inputCountry" class="sr-only">Country</label>
+            <input id="inputCountry" type="tetx" placeholder="Country" class="form-control" required>
         </div>
 
         <textarea rows="5" class="form-control" required></textarea>
         <div class="form-group margin-top-15">
             <p>Travel Professional?</p>
             <label class="radio-inline">
-                <input id="Radio3" type="radio" name="inlineRadioOptionsProfessional" value="option3" required> Yes
+                <input id="yesRadio" type="radio" name="inlineRadioOptionsProfessional" value="yes" required> Yes
             </label>
             <label class="radio-inline">
-                <input id="Radio4" type="radio" name="inlineRadioOptionsProfessional" value="option4"> No
+                <input id="noRadio" type="radio" name="inlineRadioOptionsProfessional" value="no"> No
             </label>
         </div>
 
@@ -57,21 +63,43 @@
     <script>
         this.flag = false;
 
+        this.on('mount', function(){
+            console.log('mount form with riot.js') // (mount <form> component)
+            $(this.conversationForm).validator().on('submit', this.submit);
+        })
+
+        var self = this
 
         submit(e) {
+            console.log("Submitting");
 
-            this.flag = true;
+            if (e.isDefaultPrevented()) {
+                // handle the invalid form...
+                console.log("handle the invalid form")
 
-            var data = {
-                username: this.userName.value,
-                email: this.InputEmail.value
-            };
+            } else {
+                // everything looks good!
+                console.log("everything looks good!")
+                e.preventDefault();
 
-            window.conrollers.ajax_form_controller(data);
+                // get data from controls
+                var data = {
+                    username: self.userName.value,
+                    email: self.inputEmail.value
+                };
+                // send data to server by controler
+                window.conrollers.ajax_form_controller(data);
+                // display green message "Thank for starting the conversation"
+                self.flag = true;
+                // update UI in riot.js tag
+                self.update()
+            }
+
+            return false;// stop default sending form by GET request
         }
 
         clickContinueHandler(e) {
-            this.flag = false;
+            this.flag = false;// show form enother in riot.js tag
 
             this.conversationForm.reset();
         }
